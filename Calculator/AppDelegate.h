11c9018e4915295d574e6454a4ecd2b42bdacd15
Naveen Katari <naveen.katari@sourcebits.com>
2015-10-20 18:09:55 +0530

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Naveen Katari on 20/10/15.
+//  Copyright (c) 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Naveen Katari on 20/10/15.
+//  Copyright (c) 2015 Sourcebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
